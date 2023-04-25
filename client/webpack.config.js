a40951045a4258c37a0d9c2bfb6555d4961a122d
ikaera <ikaera@gmail.com>
2023-04-24 21:20:46 -0400

    edit WebpackPwaManifest.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,13 +34,13 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Text Editor',
+        short_name: 'TE',
+        description: 'Text Editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
